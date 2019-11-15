1c8425efda1ae4bb91c8c71daa4ca989398e8f46
Noah Kernis <nkernis@gmail.com>
2019-11-15 16:09:09 -0500

    cleaned and orgamized

--- server/models/gossip.js
+++ server/models/gossip.js
@@ -3,6 +3,10 @@
 const sneeze = require('sneeze');
 const config = require('./server/config/config')
 
+// TODO: Sneeze does not have method for updating meta data. Need data to spread via gossip. Need
+// 		 to add method. Either edit node_modules, or import sneeze.js and edit it (is a single file)
+
+// NOTE: This is to be called on server startup, not for api use
 exports.bootstrap = () => {
 	let active
 	let bases = config.cluster.bases
@@ -34,41 +38,7 @@ exports.bootstrap = () => {
 	return active;
 }
 
-// Returns an array of member description objects.These are the currently known and healthy members.
-// sneeze.members()
-
-// Leave the network.You can also leave just by exiting the process - SWIM is designed to handle that.This method is just to give you extra control.
-// sneeze.leave()
-
-// This public member variable is null until the instance sucessfully joins a network.It is descriptive object containing meta data about this instance.
-// sneeze.info
-
-
-exports.getLocalState = (swim) => {
-	// swim.members(hasLocal=true, hasFaulty)
-}
-
-exports.getWhoAmI = (swim) => {
-	// swim.whoami()
-}
-
-exports.getMembers = (swim) => {
-	// swim.members(hasLocal=false, hasFaulty)
-}
-
-exports.updateLocalState = (swim) => {
-	// swim.updateMeta(meta)
-}
-
-exports.joinCluster = (swim) => {
-	// swim.join(hosts, callback)
-}
-
-exports.leaveCluster = (swim) => {
-	// swim.leave()
-}
-
-// -----> Helper Functions <-----
+// -----> Start Bootstrap Helper Functions <-----
 
 // identifier: null
 // You can provide a unique identifier for your instance.This is generated automatically if you do not provide one.
@@ -108,6 +78,8 @@ const newMember = (bases, host, port, name) => {
 	member.join({
 		name: name
 	})
+
+	return member;
 }
 
 // TODO: Change this to monitor settings or remove
@@ -119,16 +91,17 @@ const newDisplay = (bases, host, port, name) => {
 		port: port
 	};
 
-	let member = sneeze(opts)
+	let display = sneeze(opts)
 
-	member.on('add', console.log)
+	display.on('add', console.log)
 
-	member.on('remove', console.log)
+	display.on('remove', console.log)
 
-	member.join({
+	display.join({
 		name: name
 	})
 
+	return display;
 }
 
 const newMonitor = (bases, host, port, name) => {
@@ -152,39 +125,31 @@ const newMonitor = (bases, host, port, name) => {
 	monitor.join({
 		name: name
 	})
+
+	return monitor;
 }
 
-// var swim = new Swim(opts);
-// swim.bootstrap(config.cluster.hostsToJoin);
+// -----> End Bootstrap Helper Functions <-----
 
-// // bootstrap error handling
-// swim.on(Swim.EventType.Error, function onError(err) {
-// 	console.log(err);
-// });
+exports.getLocalState = (sneeze) => {
+	// This public member variable is null until the instance sucessfully joins a network.It is descriptive object containing meta data about this instance.
+	// sneeze.info
+}
 
-// // bootstrap ready
-// swim.on(Swim.EventType.Ready, function onReady() {
-// 	console.log(swim.whoami());
-// 	console.log(swim.members());
-// 	console.log(swim.checksum());
-// 	console.log("");
-// });
+exports.getMembers = (sneeze) => {
+	// Returns an array of member description objects.These are the currently known and healthy members.
+	// sneeze.members()
+}
 
-// // change in membership (new node or node died/left)
-// swim.on(Swim.EventType.Change, function onChange(update) {
-// 	console.log(swim.whoami() + " has changed: " + update)
-// });
+exports.updateLocalState = (sneeze) => {
 
-// // update in membership (node recovered or update on meta data)
-// swim.on(Swim.EventType.Update, function onUpdate(update) {
-// 	console.log(swim.whoami() + " has updated: " + update.meta.application)
-// 	console.log("")
-// });
+}
 
-// setTimeout(function () {
-// 	var meta = {
-// 		'state': 'dog'
-// 	};
+exports.joinCluster = (sneeze) => {
 
-// 	swim.updateMeta(meta)
-// }, 10000);
+}
+
+exports.leaveCluster = (sneeze) => {
+	// Leave the network.You can also leave just by exiting the process - SWIM is designed to handle that.This method is just to give you extra control.
+	// sneeze.leave()
+}
